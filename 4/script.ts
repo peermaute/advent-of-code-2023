9a6edf47bce10b612b97778bf35d39592b0d00b3
Peer Maute <peer.maute@haw-hamburg.de>
2023-12-04 16:46:52 +0100

    solution: add solution for day 4 part 2

--- 4/script.ts
+++ 4/script.ts
@@ -50,6 +50,54 @@ const getCardMatches = (card: Card) => {
   return cardMatches;
 };
 
+const getCardWinMap = (cards: Card[]) => {
+  // map with card id as key and number of wins as value
+  const cardWinMap = new Map<number, number>();
+  cards.forEach((card) => {
+    cardWinMap.set(card.id, getCardMatches(card));
+  });
+  return cardWinMap;
+};
+
+const getTotalAmountOfCards = (cardWinMap: Map<number, number>) => {
+  let totalAmountOfCards = 0;
+  // map that represents the actual won cards - key is the id of the card and value is the amount of times the card has been won
+  const actualWinMap = initializeActualWinMap(cardWinMap.size);
+  cardWinMap.forEach((cardWins, cardId) => {
+    processCard(cardId, cardWins, actualWinMap);
+  });
+  actualWinMap.forEach((cardWins) => {
+    totalAmountOfCards += cardWins;
+  });
+  return totalAmountOfCards;
+};
+
+const processCard = (
+  cardId: number,
+  cardWins: number,
+  actualWinMap: Map<number, number>
+) => {
+  const actualWins = actualWinMap.get(cardId) ?? 0;
+  // go over the amount of wins this card has up to this point
+  for (let i = 0; i < actualWins; i++) {
+    // enter another actual card win to each card that comes after the winning card based on the amount of cards this winning card wins
+    for (let j = cardId + 1; j < cardId + 1 + cardWins; j++) {
+      if (actualWinMap.has(j)) {
+        const currentWins = actualWinMap.get(j);
+        if (currentWins) actualWinMap.set(j, currentWins + 1);
+      }
+    }
+  }
+};
+
+const initializeActualWinMap = (size: number) => {
+  const actualWinMap = new Map<number, number>();
+  for (let i = 1; i <= size; i++) {
+    actualWinMap.set(i, 1);
+  }
+  return actualWinMap;
+};
+
 const partOne = () => {
   const cards: Card[] = [];
   difficultActualInput.forEach((card: string) => {
@@ -70,6 +118,9 @@ const partTwo = () => {
     cards.push(cardObject);
     console.log(cardObject);
   });
+  const cardWinMap = getCardWinMap(cards);
+  const totalAmountOfCards = getTotalAmountOfCards(cardWinMap);
+  console.log("TOTAL AMOUNT OF CARDS: " + totalAmountOfCards);
 };
 
-partOne();
+partTwo();
