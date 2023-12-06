74da71d3655856dcd52674bbc87739b630410c47
Peer Maute <peer.maute@haw-hamburg.de>
2023-12-06 08:35:55 +0100

    solution: add solution day 6 part 1

---
+++ 6/script.ts
@@ -0,0 +1,47 @@
+import { difficultActualInput } from "../6/input";
+
+type Game = {
+  time: number;
+  distance: number;
+};
+
+const getGames = (): Game[] => {
+  const games: Game[] = [];
+  const times = readInput(0);
+  const distances = readInput(1);
+  for (let i = 0; i < times.length; i++) {
+    games.push({ time: times[i], distance: distances[i] });
+  }
+  return games;
+};
+
+const readInput = (lineNumber: number): number[] => {
+  return difficultActualInput[lineNumber]
+    .split(":")[1]
+    .trim()
+    .split(" ")
+    .filter((string: string) => string !== "")
+    .map((numString: string) => parseInt(numString));
+};
+
+const getWinPossibilities = ({ time, distance }: Game): number[] => {
+  const winPossibilities: number[] = [];
+  for (let secondsHeld = 1; secondsHeld < time; secondsHeld++) {
+    const timeToTravel = time - secondsHeld;
+    if (distance < timeToTravel * secondsHeld)
+      winPossibilities.push(secondsHeld);
+  }
+  return winPossibilities;
+};
+
+const partOne = () => {
+  const games = getGames();
+  let result = 1;
+  games.forEach((game: Game) => {
+    const winPossobilities = getWinPossibilities(game);
+    result *= winPossobilities.length;
+  });
+  console.log("RESULT: " + result);
+};
+
+partOne();
