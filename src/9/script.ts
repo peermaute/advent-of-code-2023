71fbccb2744d7496605f9b6573a3eb6aa6232dc2
Peer Maute <peer.maute@haw-hamburg.de>
2023-12-09 09:48:12 +0100

    solution: add solution day 9 part 1

---
+++ src/9/script.ts
@@ -0,0 +1,45 @@
+const getNextNumber = (line: number[]) => {
+  const differences = getDifferences(line);
+  const nextNumber =
+    differences[differences.length - 1] + line[line.length - 1];
+  return nextNumber;
+};
+
+const getDifferences = (line: number[]): number[] => {
+  if (allValuesAreEqual(line)) return new Array(line.length).fill(0);
+  const differences = [];
+  for (let i = 0; i < line.length - 1; i++) {
+    differences.push(line[i + 1] - line[i]);
+  }
+  const nextDifferences = getDifferences(differences);
+  differences.push(
+    differences[differences.length - 1] +
+      nextDifferences[nextDifferences.length - 1]
+  );
+  console.log(differences, "differences");
+  return differences;
+};
+
+const allValuesAreEqual = (line: number[]): boolean => {
+  const value = line[0];
+  for (let i = 1; i < line.length; i++) {
+    if (line[i] !== value) return false;
+  }
+  return true;
+};
+
+export const partOne = (input: string[]) => {
+  const inputLines = input.map((line) =>
+    line.split(" ").map((n) => parseInt(n))
+  );
+  let result = 0;
+  inputLines.forEach((line) => {
+    const nextNumber = getNextNumber(line);
+    result += nextNumber;
+  });
+  console.log(result, "result");
+};
+
+export const partTwo = (input: string[]) => {
+  //logic
+};
